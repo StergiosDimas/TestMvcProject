c678cf564984e5af02ee91138487e82300547dd2
Vinnie Moore <sdimas14@gmail.com>
2019-06-05 14:50:31 +0300

    AddStudentModel

---
+++ BootCampTests/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootCampTests.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Age { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
