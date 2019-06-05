03f5996db635dca2e6763a9c02a08698c2976b14
Vinnie Moore <sdimas14@gmail.com>
2019-06-05 15:17:37 +0300

    Revert "Revert "Create_Student""
    This reverts commit bd875418086fb656c0a205fc9e297e01358b8a04.

---
+++ BootCampTests/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootCampTests.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateAdded { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+
+    }
+}
